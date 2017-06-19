cae8568debcd4e48e8419a1010afaa7ab532d066
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2017-06-18 22:14:50 -0300

    adiciona placeholder para os campos de peso e altura

--- js/application.js
+++ js/application.js
@@ -28,20 +28,29 @@ function calculeImc(peso, altura) {
 }
 
 function setIMCStat(imcValue) {
+  $('#imc').removeClass();
+  $('#imc').addClass('imc-field');
+
   var message = ''
   var imc = Math.round(imcValue * 100) / 100;
   if (imc < 18.5) {
     message = 'Baixo Peso';
+    $('#imc').addClass('is-bad');
   } else if (imc >= 18.5 && imc < 25) {
     message = 'Peso Ideal';
+    $('#imc').addClass('is-good');
   } else if (imc >= 25 && imc < 30) {
     message = 'Sobrepeso';
+    $('#imc').addClass('is-worrying');
   } else if (imc >= 30 && imc < 35) {
     message = 'Obesidade grau 1';
+    $('#imc').addClass('is-bad');
   } else if (imc >= 35 && imc < 40) {
     message = 'Obesidade grau 2';
+    $('#imc').addClass('is-bad');
   } else if (imc > 40) {
     message = 'Obesidade grau 3';
+    $('#imc').addClass('is-bad');
   } else {
     message = 'não encontrado';
   }
